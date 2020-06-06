686a4b2c78a6d4f402ff865ca0bd72225cfe076f
marklawrence97 <mark_lawrence97@icloud.com>
2020-06-06 17:26:52 +0100

    configure skaffold

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,18 +12,18 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post("http://localhost:4000/events", event);
-    axios.post("http://localhost:4001/events", event);
-    axios.post("http://localhost:4002/events", event);
-    axios.post("http://localhost:4003/events", event);
+    axios.post("http://posts-clusterip-srv:4000/events", event);
+    axios.post("http://comments-srv:4001/events", event);
+    axios.post("http://query-srv:4002/events", event);
+    axios.post("http://moderation-srv:4003/events", event);
 
     res.send({ status: "OK"})
 });
 
 app.get('events', (req, res) => {
     res.send(events);
-})
+});
 
 app.listen(4005, () => {
     console.log("Listening on 4005")
-})
+});
--- posts/index.js
+++ posts/index.js
@@ -14,15 +14,15 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
-    const title = req.body.title
+    const title = req.body.title;
 
     posts[id] = {
         id, title
     };
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
         type: "PostCreated",
         data: {
             id, title
@@ -38,5 +38,5 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log("Listening on 4000")
-})
+    console.log("Listening on 4000");
+});
